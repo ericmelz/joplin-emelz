397a4d7d20d167cfa6068df545f4f4ce7e8ee1f6
ericmelz <eric@emelz.com>
2025-09-28 13:06:05 -0700

    work

---
+++ Makefile
@@ -0,0 +1,5 @@
+.PHONY: k3d
+
+k3d:
+	bash scripts/k3d.sh
+
---
+++ k3d.sh
@@ -0,0 +1,4 @@
+Script started on Sun Sep 28 13:04:50 2025
+[1m[7m%[27m[1m[0m                                                                                ]7;file://homer/Users/ericmelz/Data/code/joplin-emelz[0m[27m[24m[Jericmelz@homer joplin-emelz % [K[?2004hsscripts/s k3d.sh[1m [0m[0m [?2004l
+CLUSTER_NAME=dev
+[1m[7m%[27m[1m[0m                                                                                ]7;file://homer/Users/ericmelz/Data/code/joplin-emelz[0m[27m[24m[Jericmelz@homer joplin-emelz % [K[?2004h
